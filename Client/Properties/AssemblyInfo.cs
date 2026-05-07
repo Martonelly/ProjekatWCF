9c982c61dc0b38fce553ade5c9ea0d4b968569b1
Marton Viktor Melar <melar.pr9.2023@uns.ac.rs>
2026-05-07 13:14:12 +0200

    Added project structure
    Deleted initial project

--- Client/Properties/AssemblyInfo.cs
+++ Client/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Project")]
+[assembly: AssemblyTitle("Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Project")]
+[assembly: AssemblyProduct("Client")]
 [assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1bf8b442-4d29-40fb-9f2c-3dd7c2b8c2da")]
+[assembly: Guid("455dbd0f-3507-4695-95af-96b22cbe48b7")]
 
 // Version information for an assembly consists of the following four values:
 //
