e8fd21220a6a84f982220eb246f39a258f39343e
Dobi04 <nj675896@gmail.com>
2026-05-11 22:44:24 +0200

    Changed logic for reading from CSV and added Dispose pattern

--- Client/Functions/ReadFromDataBase.cs
+++ Client/Functions/ReadFromDataBase.cs
@@ -9,32 +9,78 @@ using System.Threading.Tasks;
 
 namespace Client.Functions
 {
-    public class ReadFromDataBase : IReadFromDataBase
+    public class ReadFromDataBase : IReadFromDataBase, IDisposable
     {
-        public void FReadFromDataBase(string path)
+        private FileStream fs;
+        private StreamReader sr;
+        private bool disposedValue = false;
+
+        public ReadFromDataBase(string path)
+        {
+            fs = File.OpenRead(path);
+            sr = new StreamReader(fs);
+        }
+
+        public List<PvSample> FReadFromDataBase(string path, int limitN)
         {
             using (FileStream fs = File.OpenRead(path))
             {
                 using (StreamReader sr = new StreamReader(fs))
                 {
+                    string headerLine = sr.ReadLine(); // Skip the header line
                     string line;
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        string[] parts = line.Split(',');
-                        int Day = int.Parse(parts[1]);
-                        string Hour = parts[2];
-                        double AcPwrt = double.Parse(parts[3]);
-                        double DcVolt = double.Parse(parts[4]);
-                        double Temper = double.Parse(parts[6]);
-                        double Vl1to2 = double.Parse(parts[7]);
-                        double Vl2to3 = double.Parse(parts[8]);
-                        double Vl3to1 = double.Parse(parts[9]);
-                        double AcCur1 = double.Parse(parts[10]);
-                        double AcVlt1 = double.Parse(parts[13]);
+                    List<PvSample> samples = new List<PvSample>();
+                    int rowIndex = 0;
 
-                        PvSample pvSample = new PvSample(Day, Hour, AcPwrt, DcVolt, Temper, Vl1to2, Vl2to3, Vl3to1, AcCur1, AcVlt1, int.Parse(parts[0]));
+                    while ((line = sr.ReadLine()) != null && rowIndex < limitN)
+                    {
+                        try
+                        {
+                            string[] parts = line.Split(',');
+                            int Day = int.Parse(parts[1]);
+                            string Hour = parts[2];
+                            double AcPwrt = double.Parse(parts[3]);
+                            double DcVolt = double.Parse(parts[4]);
+                            double Temper = double.Parse(parts[6]);
+                            double Vl1to2 = double.Parse(parts[7]);
+                            double Vl2to3 = double.Parse(parts[8]);
+                            double Vl3to1 = double.Parse(parts[9]);
+                            double AcCur1 = double.Parse(parts[10]);
+                            double AcVlt1 = double.Parse(parts[13]);
+
+                            PvSample pvSample = new PvSample(Day, Hour, AcPwrt, DcVolt, Temper, Vl1to2, Vl2to3, Vl3to1, AcCur1, AcVlt1, int.Parse(parts[0]));
+                            for (int i = 0; i < limitN; i++)
+                            {
+                                samples.Add(pvSample);
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            File.AppendAllText("rejected_client.CSV", line + Environment.NewLine);
+                        }
                     }
+                    return samples;
                 }
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    sr.Dispose();
+                    fs.Dispose();
+                    Console.WriteLine("Clients resources has being releaced");
+                }
+                disposedValue = true;
             }
         }
     }
