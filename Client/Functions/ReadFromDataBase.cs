be7c8378d498a0a82a6b72eac73d5f956fbaf687
Dobi04 <nj675896@gmail.com>
2026-05-11 22:14:41 +0200

    Added: 1. Diagram 2. Interface for reading from CVS file 3. function for reading from CSV file

---
+++ Client/Functions/ReadFromDataBase.cs
@@ -0,0 +1,41 @@
+﻿using Common.Contracts;
+using Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Functions
+{
+    public class ReadFromDataBase : IReadFromDataBase
+    {
+        public void FReadFromDataBase(string path)
+        {
+            using (FileStream fs = File.OpenRead(path))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] parts = line.Split(',');
+                        int Day = int.Parse(parts[1]);
+                        string Hour = parts[2];
+                        double AcPwrt = double.Parse(parts[3]);
+                        double DcVolt = double.Parse(parts[4]);
+                        double Temper = double.Parse(parts[6]);
+                        double Vl1to2 = double.Parse(parts[7]);
+                        double Vl2to3 = double.Parse(parts[8]);
+                        double Vl3to1 = double.Parse(parts[9]);
+                        double AcCur1 = double.Parse(parts[10]);
+                        double AcVlt1 = double.Parse(parts[13]);
+
+                        PvSample pvSample = new PvSample(Day, Hour, AcPwrt, DcVolt, Temper, Vl1to2, Vl2to3, Vl3to1, AcCur1, AcVlt1, int.Parse(parts[0]));
+                    }
+                }
+            }
+        }
+    }
+}
